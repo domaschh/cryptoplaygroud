f770efcb109be40b16eae7cac732bd672c9d4f71
lukas <lukas.sichert@gmail.com>
2024-02-28 18:27:43 +0100

    newsub

--- src/lib.rs
+++ src/lib.rs
@@ -13,12 +13,12 @@ mod tests {
     #[test]
     fn it_works() {
         let result = add(2, 2);
-        assert_eq!(result, 5);
+        assert_eq!(result, 4);
     }
 
     #[test]
     fn subtract_works() {
-        let result = subtract(3, 3);
-        assert_eq!(result, 0)
+        let result = subtract(3, 2);
+        assert_eq!(result, 1)
     }
 }
