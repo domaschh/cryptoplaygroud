bed7aa0c1fff2ac06f2ecd904011e14560dcf8f1
lukas <lukas.sichert@gmail.com>
2024-02-28 18:14:45 +0100

    newsubtract

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,7 @@ mod tests {
 
     #[test]
     fn subtract_works() {
-        let result = subtract(3, 2);
-        assert_eq!(result, 1)
+        let result = subtract(3, 3);
+        assert_eq!(result, 0)
     }
 }
