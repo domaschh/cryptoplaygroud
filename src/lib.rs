3e67f963ddf8af585b42208510429f98a737df62
lukas <lukas.sichert@gmail.com>
2024-02-29 21:44:35 +0100

    revision

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+use rand::Rng;
+
+
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
@@ -12,6 +16,8 @@ pub fn multiply(a: i32, b: i32) -> i32 {
 
 #[cfg(test)]
 mod tests {
+    use std::cmp::Ordering;
+
     use super::*;
 
     #[test]
@@ -24,5 +30,14 @@ mod tests {
     fn subtract_works() {
         let result = subtract(3, 1);
         assert_eq!(result, 2)
+    }
+    #[test]
+    fn randfun() {
+        let random_number= rand::thread_rng().gen_range(1..=100);
+        match random_number.cmp(&1){
+            Ordering::Greater => print!("Rust is doch ganz nice bisher \n"),
+            Ordering::Equal => print!("(bin auf seite 3)\n"),
+            Ordering::Less => print!("Rust is kacke\n")
+        }
     }
 }
